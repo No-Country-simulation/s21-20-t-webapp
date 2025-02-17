f265b1df995de3919fb4d81806acdf42635cb97f
dewinson1 <dewinson12@gmail.com>
2025-02-17 16:49:02 -0400

    Carpetass

---
+++ Frontend/SaasWebApp/src/Interfaces/interfaces/interfaces.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interfaces',
+  imports: [],
+  templateUrl: './interfaces.component.html',
+  styleUrl: './interfaces.component.css'
+})
+export class InterfacesComponent {
+
+}
