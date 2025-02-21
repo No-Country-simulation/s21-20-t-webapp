c45bef7200cd7e9d4d4e1709280397162c4bca5b
JuanEstebanLG <56986294+JuanEstebanLG@users.noreply.github.com>
2025-02-20 23:25:18 -0500

    Revert "Dev backend"

--- demo/src/main/java/com/inventario/demo/config/exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.inventario.demo.config.exceptions;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
--- demo/src/main/java/com/inventario/demo/entities/user/Enum/EnumRole.java
+++
@@ -1,6 +0,0 @@
-package com.inventario.demo.entities.user.Enum;
-
-public enum EnumRole {
-    ADMIN,
-    USER
-}
