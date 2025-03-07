d5816ccfc00d91041d0656eccef5bbc6044a60b2
Seba Olima <edificarvcp@gmail.com>
2025-03-06 21:01:34 -0300

    se sacan los botones de register y login del nav y se agregan en la card del login

--- Frontend/SaasWebApp/src/app/pages/login/login.component.ts
+++ Frontend/SaasWebApp/src/app/pages/login/login.component.ts
@@ -1,26 +1,39 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../Services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
+  private loginSubscription!: Subscription;
+  isLoggedIn = false;
   email = '';
   password = '';
   errorMessage = '';
 
   constructor(private authService: AuthService, private router: Router) {}
+  ngOnInit() {
+    this.loginSubscription = this.authService.getLoginStatus().subscribe(
+      (isLoggedIn) => {
+        this.isLoggedIn = isLoggedIn;
+      }
+    );}
 
   login() {
     this.authService.login(this.email, this.password).subscribe({
       next: () => this.router.navigate(['/dashboard']),
       error: (err) => (this.errorMessage = 'Error en el inicio de sesión'),
     });
+  }
+
+  register(): void {
+    this.router.navigate(['/register']);
   }
 }
